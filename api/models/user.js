e10dbc374dc38613e84bbd8fc660fe6b778db423
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2024-02-29 08:39:54 +0530

    added user.js
    Backend work

---
+++ api/models/user.js
@@ -0,0 +1,51 @@
+const { Token } = require("@mui/icons-material");
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String,
+        required: true
+    },
+    joinDate: {
+        type: Date,
+        default: Date.now
+    },
+    sentFollowRequest: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+
+    recivedFollowingRequest: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    verified:{
+        type: Boolean,
+        default: false
+    },
+    verificationToken: String,
+
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
