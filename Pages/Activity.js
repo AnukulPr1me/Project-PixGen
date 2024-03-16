f73574c82f77351d55869114e946aa9a6f3adccb
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2024-03-17 00:49:19 +0530

    activity page

--- Pages/Activity.js
+++ Pages/Activity.js
@@ -1,10 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
 const Activity = () => {
+  const [selectButton, setSelectButton] = useState('people');
+  const [content, setContent] = useState('people_content');
+  
   return (
-    <View>
-      <Text>Activity</Text>
+    <View style={{ marginTop: 10 }}>
+      <Text style={{ fontSize: 25, fontWeight: 'bold', color: 'black' }}>
+        Activity
+      </Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TouchableOpacity
+          style={[
+            {
+              flex: 1,
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              borderRadius: 10,
+              borderWidth: 1,
+              backgroundColor: 'white',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+            selectButton === 'people' ? { backgroundColor: 'black' } : null,
+          ]}
+          onPress={() => setSelectButton('people')}>
+          <Text style={[{ textAlign: 'center', fontWeight: 'bold' }, selectButton === 'people' ? { color: 'white' } : { color: 'black' }]}>
+            People
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
-export default Activity
-const styles = StyleSheet.create({})
+  );
+};
+
+export default Activity;
+const styles = StyleSheet.create({});
